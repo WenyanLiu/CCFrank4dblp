f2ed596896c89b157da1c48454167e0007223949
Wang Zi-cheng <wzc@smail.nju.edu.cn>
2021-08-18 20:49:13 +0800

    get after the page has been loaded

--- js/connectedpapers.js
+++ js/connectedpapers.js
@@ -10,18 +10,22 @@ connectedpapers.rankSpanList = [];
 
 connectedpapers.run = function () {
     let url = window.location.pathname;
-    window.alert(url);
-    // let elements = $("#desktop-app > div.list-group-item-mod.minilist-list-entry");
-    // let elements = $("div.minilist-column.flexcolumn");
-    let elements = $(".minilist-column.flexcolumn")
-    // let elements = $(this).find("div.main-view-window");
-
-    // elements.each(function () {
-    //     console.log($(this));
+    window.onload=function(){
+        // window.alert(url);
+        // let elements = $("#desktop-app > div.flexcolumn.main-router > div.main-view-window");
+        let elements = $("div.minilist-column.flexcolumn");
     //     console.log("trigger");
-    // });
-    console.log("trigger");
-    console.log(elements);
+        console.log(elements);
+        let origin = $("div.list-group-item-mod.minilist-main-paper.main");
+        console.log(origin);
+        let connects = $("div.list-group-item-mod.minilist-list-entry");
+        console.log(connects);
+    };
+    
+    
+
+    console.log("trigger1");
+
 };
 
 // var importJs=document.createElement('script');importJs.setAttribute("type","text/javascript");importJs.setAttribute("src", 'https://ajax.microsoft.com/ajax/jquery/jquery-1.4.min.js');document.getElementsByTagName("head")[0].appendChild(importJs);
