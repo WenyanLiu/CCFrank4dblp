fed6879c3626222e869f03d5911a9e57044dff17
wyliu <f.v.rockie@hotmail.com>
2023-11-29 19:23:21 +0800

    Added copyright information for Pull request author mra42

--- js/scholar.js
+++ js/scholar.js
@@ -1,7 +1,7 @@
 /**
  * MIT License
  *
- * Copyright (c) 2019-2023 WenyanLiu (https://github.com/WenyanLiu/CCFrank4dblp)
+ * Copyright (c) 2019-2023 WenyanLiu (https://github.com/WenyanLiu/CCFrank4dblp), mra42 (https://github.com/mra42)
  */
 
 const scholar = {};
