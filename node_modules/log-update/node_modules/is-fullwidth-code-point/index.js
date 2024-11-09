1c911bf048aece30a65bc33f635672bf387000ca
wyliu <f.v.rockie@hotmail.com>
2024-11-09 09:44:11 +0800

    Delete node_modules directory

--- node_modules/log-update/node_modules/is-fullwidth-code-point/index.js
+++
@@ -1,9 +0,0 @@
-import {eastAsianWidth} from 'get-east-asian-width';
-
-export default function isFullwidthCodePoint(codePoint) {
-	if (!Number.isInteger(codePoint)) {
-		return false;
-	}
-
-	return eastAsianWidth(codePoint) === 2;
-}
